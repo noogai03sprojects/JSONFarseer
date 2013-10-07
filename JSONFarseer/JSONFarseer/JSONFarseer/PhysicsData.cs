ccb569b438092343fb87da1e128bbef9341e7f9e
noogai03 <0xzeroex@gmail.com>
2013-10-07 20:48:26 +0100

    Added shell classes for physics info and player.

---
+++ JSONFarseer/JSONFarseer/JSONFarseer/PhysicsData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSONFarseer
+{
+    class PhysicsData
+    {
+        public List<PhysicsCircle> Circles;
+        public List<PhysicsRectangle> Rectangles;
+
+        public PhysicsData()
+        {
+            Circles = new List<PhysicsCircle>();
+            Rectangles = new List<PhysicsRectangle>();
+        }
+    }
+}
---
+++ JSONFarseer/JSONFarseer/JSONFarseer/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FarseerPhysics.Dynamics;
+
+namespace JSONFarseer
+{
+    class Player : PhysicsObject
+    {
+        public static Player Instance;        
+    }
+}
